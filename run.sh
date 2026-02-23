f2bcc9a21f7ee2d3970696707b97e495b84b7c3a
Adminiculum <fadvka@gmail.com>
2026-02-23 01:13:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Activate the virtual environment
-source venv/bin/activate
+# Activate the new virtual environment
+source AIvenv/bin/activate
 
 # Run the Streamlit application
 streamlit run app.py
+
+# Optionally deactivate
+# deactivate
